1fae3933d36e2505afc2943e19a576f3dd78529f
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-17 10:12:06 +0700

    :money_with_wings: Change DB

--- server/src/user/user.resolver.ts
+++ server/src/user/user.resolver.ts
@@ -5,30 +5,25 @@ import { UpdateUserInput } from './dto/update-user.input';
 
 @Resolver('api/user')
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
-
-  @Mutation('add')
-  create(@Args('createUserInput') createUserInput: CreateUserInput) {
-    return this.userService.createUser(createUserInput);
-  }
-
-  @Query('user')
-  findAll() {
-    return this.userService.findAll();
-  }
-
-  @Query('user')
-  findOne(@Args('id') id: string) {
-    return this.userService.findOne(id);
-  }
-
-  // @Mutation('updateUser')
-  // update(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
-  //   return this.userService.update(updateUserInput.id, updateUserInput);
+  // constructor(private readonly userService: UserService) {}
+  // @Mutation('add')
+  // create(@Args('createUserInput') createUserInput: CreateUserInput) {
+  //   return this.userService.createUser(createUserInput);
   // }
-
-  @Mutation('removeUser')
-  remove(@Args('id') id: string) {
-    return this.userService.remove(id);
-  }
+  // @Query('user')
+  // findAll() {
+  //   return this.userService.findAll();
+  // }
+  // @Query('user')
+  // findOne(@Args('id') id: string) {
+  //   return this.userService.findOne(id);
+  // }
+  // // @Mutation('updateUser')
+  // // update(@Args('updateUserInput') updateUserInput: UpdateUserInput) {
+  // //   return this.userService.update(updateUserInput.id, updateUserInput);
+  // // }
+  // @Mutation('removeUser')
+  // remove(@Args('id') id: string) {
+  //   return this.userService.remove(id);
+  // }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -6,40 +6,33 @@ import { FindUserInput } from './inputs/user.input';
 
 @Injectable()
 export class UserService {
-  constructor(
-    private prisma: PrismaService,
-    @InjectModel(User.email) private UserModel: Model<User>,
-  ) {}
-
-  async createUser(createUserInput: CreateUserInput) {
-    const user = await this.prisma.user.create({
-      data: {
-        ...createUserInput,
-      },
-    });
-
-    const data = {
-      message: 'success',
-      status: 'success',
-      data: user,
-    };
-
-    return data;
-  }
-
-  async findAll(user: FindUserInput): Promise<user> {
-    return `This action returns all user`;
-  }
-
-  findOne(id: string) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: string, updateUserInput: UpdateUserInput) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: string) {
-    return `This action removes a #${id} user`;
-  }
+  // constructor(
+  //   private prisma: PrismaService,
+  //   @InjectModel(User.email) private UserModel: Model<User>,
+  // ) {}
+  // async createUser(createUserInput: CreateUserInput) {
+  //   const user = await this.prisma.user.create({
+  //     data: {
+  //       ...createUserInput,
+  //     },
+  //   });
+  //   const data = {
+  //     message: 'success',
+  //     status: 'success',
+  //     data: user,
+  //   };
+  //   return data;
+  // }
+  // async findAll(user: FindUserInput): Promise<user> {
+  //   return `This action returns all user`;
+  // }
+  // findOne(id: string) {
+  //   return `This action returns a #${id} user`;
+  // }
+  // update(id: string, updateUserInput: UpdateUserInput) {
+  //   return `This action updates a #${id} user`;
+  // }
+  // remove(id: string) {
+  //   return `This action removes a #${id} user`;
+  // }
 }
