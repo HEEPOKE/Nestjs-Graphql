01fdc765be7efda2c9223bc8c8959e6f9b864aa1
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-17 09:11:49 +0700

    :money_with_wings: Update Module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -14,6 +14,7 @@ import { UserModule } from './user/user.module';
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
+      autoSchemaFile: true,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault()],
       typePaths: ['./**/*.graphql'],
